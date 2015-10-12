1b8c22a4cca4144bfde691125c093556c2505a18
rubenmunozgit <ruben_munoz_palomino@hotmail.com>
2015-10-12 22:50:05 +0200

    sails mongo online connection

--- config/connections.js
+++ config/connections.js
@@ -62,7 +62,7 @@ module.exports.connections = {
     host: 'ds029824.mongolab.com',//'localhost',
     port: 29824,//27017,
     user: 'heroku_1g0l4g9p',//'username',
-    password: 'KrTNtv9NtUDCl+2bmLWz9A==',//'password',
+    password: 'Pmdo5Y_cpf0SbggZ6F2zNolWDyAbvR2O',//'password',
     database: 'heroku_1g0l4g9p'//'your_mongo_db_name_here'
   },
 
